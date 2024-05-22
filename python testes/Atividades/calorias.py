76e8ed4c56c840cff82884bbf01788448e90d14d
Kamila Oliveira Costa <153563504+KamsCosta@users.noreply.github.com>
2024-05-22 12:41:19 -0300

    Delete python testes directory

--- python testes/Atividades/calorias.py
+++
@@ -1,7 +0,0 @@
-quantidade_alimento = int(input("Informe quantide de alimento consumida hoje: "))
-total_calorias = 0
-
-for alimento in range(1, quantidade_alimento + 1, 1):
-    calorias = int(input(f"Informe as calorias do {alimento} alimento: "))
-    total_calorias = total_calorias + calorias
-print(f"As calorias consumidas ao logo do dia fora {total_calorias} calorias")
--- python testes/Entedendo Somas/velocidade_patinete.py
+++
@@ -1,6 +0,0 @@
-print("Esse programa calcula a velocidade média de um patinete")
-distancia = input("Qual foi a distancia em metros percorrida pelo patinete? ")
-tempo = input(
-    "Quantos minutos o patinete demorou para percorrer essas distância? ")
-velocidade_media = float(distancia) / float(tempo)
-print("O patinete atingiu uma velocidade de {}m/min".format(velocidade_media))
